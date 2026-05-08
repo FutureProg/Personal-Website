2c894b4de40f6c2e777cd81dfd468aa8ab9760cf
Nick Morrison <nickmorrison09@gmail.com>
2026-05-08 14:50:04 -0400

    Added Github Activity Event type and client hook

---
+++ src/client/hooks/useGithubActivity.ts
@@ -0,0 +1,44 @@
+import type { GithubActivityData, GithubActivityEvent } from "common/GithubActivityEvent";
+import { useEffect, useState } from "react";
+
+/**
+ * Custom hook to fetch and manage GitHub activity data.
+ * @returns An object containing the GitHub activity data and the connection status.
+ */
+export const useGithubActivity = () => {
+    const [connectionStatus, setConnectionStatus] = useState<"initializing" | "connected" | "error" | "closed">("initializing");
+    const [activity, setActivity] = useState<Record<string, GithubActivityData>>({});
+    useEffect(() => {
+        // This hook is currently a placeholder for any future side effects related to GitHub activity.
+        // For example, you might want to set up a polling mechanism to fetch the latest commits or
+        // subscribe to a WebSocket for real-time updates. For now, it does nothing.
+        const es = new EventSource("/api/github/activity");
+        es.onerror = (error) => {
+            console.debug("Error connecting to GitHub activity stream:", error);            
+            setConnectionStatus("error");        
+        }
+        es.onopen = () => {
+            console.debug("Connected to GitHub activity stream");
+            setConnectionStatus("connected");
+        }
+        es.onmessage = (event) => {
+            console.debug("Received GitHub activity:", event.data);
+            // You can add logic here to update your application's state based on the received data.
+            const payload = JSON.parse(event.data) as GithubActivityEvent;
+            if (payload.type === "initial") {
+                setActivity(payload.data);
+            } else if (payload.type === "update") {
+                setActivity((prev) => ({
+                    ...prev,
+                    [payload.data.repository]: payload.data
+                }));
+            }
+        };
+        return () => {
+            es.close();
+            console.debug("Closed connection to GitHub activity stream");
+            setConnectionStatus("closed");
+        };
+    }, []);
+    return { activity, connectionStatus };
+};
