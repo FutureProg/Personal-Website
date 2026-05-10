be2ee5e212e7e4b7f1e28e8791eb6b22a8d6c294
Nick Morrison <nickmorrison09@gmail.com>
2026-05-09 22:56:24 -0400

    Enhance GithubActivityView and stories with additional sample repositories and state management for dynamic activity updates

--- src/client/Views/GithubActivityView.stories.tsx
+++ src/client/Views/GithubActivityView.stories.tsx
@@ -3,6 +3,7 @@ import {mocked} from 'storybook/test';
 
 import { GithubActivityView } from './GithubActivityView';
 import { useGithubActivity } from '../hooks/useGithubActivity';
+import React, { useState } from 'react';
 
 const sampleRepositories = [
     {
@@ -17,6 +18,27 @@ const sampleRepositories = [
         repositoryUrl: 'https://github.com/FutureProg/repository-name-2',
         commitId: 'b8dsa8d3f1a2c4e5f6789012345678901234567',
         commitUrl: 'https://github.com/FutureProg/repository-name-2/commit/b8dsa8d3f1a2c4e5f6789012345678901234567',
+        commitTimestamp: '2028-09-09T17:00:00-05:00',
+    },
+    {
+        repositoryName: 'FutureProg/repository-name-#3',
+        repositoryUrl: 'https://github.com/FutureProg/repository-name-3',
+        commitId: 'b8dsa8d3f1a2c4e5f6789012345678901234567',
+        commitUrl: 'https://github.com/FutureProg/repository-name-3/commit/b8dsa8d3f1a2c4e5f6789012345678901234567',
+        commitTimestamp: '2028-09-09T17:00:00-05:00',
+    },
+    {
+        repositoryName: 'FutureProg/repository-name-#4',
+        repositoryUrl: 'https://github.com/FutureProg/repository-name-4',
+        commitId: 'b8dsa8d3f1a2c4e5f6789012345678901234567',
+        commitUrl: 'https://github.com/FutureProg/repository-name-4/commit/b8dsa8d3f1a2c4e5f6789012345678901234567',
+        commitTimestamp: '2028-09-09T17:00:00-05:00',
+    },
+    {
+        repositoryName: 'FutureProg/repository-name-#5',
+        repositoryUrl: 'https://github.com/FutureProg/repository-name-5',
+        commitId: 'b8dsa8d3f1a2c4e5f6789012345678901234567',
+        commitUrl: 'https://github.com/FutureProg/repository-name-5/commit/b8dsa8d3f1a2c4e5f6789012345678901234567',
         commitTimestamp: '2028-09-09T17:00:00-05:00',
     },
 ];
@@ -46,9 +68,9 @@ const meta = {
                     connectionStatus: 'closed',
                 }));
             }
-            else if (name === 'Online' || name === 'FullFeed') {
+            else if (name === 'Online') {
                 mocked(useGithubActivity).mockImplementation(() => ({
-                    items: sampleFeed,
+                    items: sampleFeed.slice(0, 1),
                     connectionStatus: 'connected',
                 }));
             }
@@ -66,6 +88,38 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Online: Story = {
+    parameters: {
+        layout: 'padded'
+    },
+    render: (args, context) => {
+        return React.createElement(() => {
+            const [activities, setActivities] = useState(sampleFeed.slice(0, 1));
+            const addActivity = () => {                
+                if (activities && activities.length >= sampleFeed.length) return;
+                const newItem = sampleFeed[activities.length];
+                document.startViewTransition?.(() => {
+                    setActivities((prev) => [newItem!, ...prev]);
+                });
+            }
+            const removeActivity = () => {
+                document.startViewTransition?.(() => {
+                    setActivities((prev) => prev.slice(0, prev.length - 1));
+                });
+            }
+            mocked(useGithubActivity).mockImplementation(() => ({
+                items: activities,
+                connectionStatus: 'connected',
+            }));
+
+            return (<div style={{display: 'grid', gridTemplateRows: 'auto 1fr', justifyContent: 'center'}}>
+                <div style={{display: 'flex', 'justifyContent': 'center'}}>
+                    <button onClick={addActivity}>Add Activity</button>
+                    <button onClick={removeActivity}>Remove Activity</button>
+                </div>
+                <GithubActivityView />
+            </div>);
+        });
+    }
 };
 
 export const Offline: Story = {
