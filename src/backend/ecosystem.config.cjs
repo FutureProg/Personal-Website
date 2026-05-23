3ee39465d2aa2c35a8a5259f0f0402640c8a0221
Nick Morrison <nickmorrison09@gmail.com>
2026-05-23 12:20:35 -0400

    Modified deployment to use ecosystem.config.cjs file, and added Github related env vars

--- src/backend/ecosystem.config.cjs
+++ src/backend/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'personal-website-api',
-      script: 'dist/index.js',
+      script: './index.js',
       instances: 1,
       autorestart: true,
       watch: false,
