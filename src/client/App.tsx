b486742c77e4b54badaab1e219262654fc67bd32
Nick Morrison <nickmorrison09@gmail.com>
2026-06-02 20:51:05 -0400

    Refine hero section text and adjust CSS class for better layout

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -21,8 +21,8 @@ export const App = () => {
                         </h1>
                         <span className='subtitle'>Full Stack Software Developer — Burlington, ON</span>
                         <div>
-                            I'm a full stack developer with a decade across the stack. <strong>I care as much about the user's experience 
-                            as the code behind it</strong>, because long-term, you can't have one without the other.
+                            I'm a full stack developer with a decade across the stack. <strong>I care as much about creating a good user's experience 
+                            as having good code behind it</strong>, because long-term, you can't have one without the other.
                         </div>
                     </div>
                     <HeroPhotoFrame />
