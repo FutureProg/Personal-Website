a46f2b66c076b17c84d60e86b2007653bb070b41
Nick Morrison <nickmorrison09@gmail.com>
2026-06-02 21:17:34 -0400

    layout adjustment

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -20,7 +20,7 @@ export const App = () => {
                             <span className={styles.lastName}>Morrison</span>
                         </h1>
                         <span className='subtitle'>Full Stack Software Developer — Burlington, ON</span>
-                        <div>
+                        <div className={styles.bio}>
                             I'm a full stack developer with a decade across the stack. <strong>I care as much about creating a good user's experience 
                             as having good code behind it</strong>, because long-term, you can't have one without the other.
                         </div>
